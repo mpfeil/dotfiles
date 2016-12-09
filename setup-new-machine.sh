9186d5b03b5c794a54bd458c765f35bedf5cb53d
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-12-09 16:20:22 +0100

    default macosx settings and setup script

---
+++ setup-new-machine.sh
@@ -0,0 +1,81 @@
+# copy paste this file in bit by bit.
+# don't run it.
+  echo "do not run this script in one go. hit ctrl-c NOW"
+  read -n 1
+
+##############################################################################################################
+### XCode Command Line Tools
+#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
+
+if ! xcode-select --print-path &> /dev/null; then
+
+    # Prompt user to install the XCode Command Line Tools
+    xcode-select --install &> /dev/null
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until the XCode Command Line Tools are installed
+    until xcode-select --print-path &> /dev/null; do
+        sleep 5
+    done
+
+    print_result $? 'Install XCode Command Line Tools'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Point the `xcode-select` developer directory to
+    # the appropriate directory from within `Xcode.app`
+    # https://github.com/alrra/dotfiles/issues/13
+
+    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+    print_result $? 'Make "xcode-select" developer directory point to Xcode'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Prompt user to agree to the terms of the Xcode license
+    # https://github.com/alrra/dotfiles/issues/10
+
+    sudo xcodebuild -license
+    print_result $? 'Agree with the XCode Command Line Tools licence'
+
+fi
+###
+##############################################################################################################
+
+##############################################################################################################
+### homebrew!
+
+# (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
+mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
+export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
+
+# install all the things
+./install/brew.sh
+./install/brew-cask.sh
+
+### end of homebrew
+##############################################################################################################
+
+
+##############################################################################################################
+### remaining configuration
+###
+
+# set up osx defaults
+#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
+sh ./os/.macosx
+
+# setup and run Rescuetime!
+
+###
+##############################################################################################################
+
+##############################################################################################################
+### symlinks to link dotfiles into ~/
+###
+
+# symlink it up!
+./install.sh
+
+###
+##############################################################################################################
